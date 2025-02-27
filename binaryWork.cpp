529da49737f1d83985d15db015334172e1f5d07c
John Doe <Emaq-r@mail.ru>
2025-02-27 18:02:18 +0600

    add a work with file

--- binaryWork.cpp
+++ binaryWork.cpp
@@ -2,6 +2,35 @@
 #include <fstream>
 #include <string>
 
+typedef struct {
+    size_t a;
+    size_t b;
+} AB;
+
+AB fileBinary(char binary_a[], char binary_b[], int bitsize) {
+    std::string line;
+    std::ifstream in("binary.txt");
+
+    AB result;
+
+    if (in.is_open())
+    {
+        if (std::getline(in, line)) {
+            std::strncpy(binary_a, line.c_str(), bitsize);
+        }
+
+        if (std::getline(in, line)) {
+            std::strncpy(binary_b, line.c_str(), bitsize);
+        }
+    }
+    in.close();
+
+    result.a = std::strlen(binary_a);
+    result.b = std::strlen(binary_b);
+
+    return result;
+}
+
 size_t inBinary(char binary[]) {
     std::cout << "Введите двоичное число (максимум 8 символов)" << std::endl;
     std::cin >> binary;
@@ -9,13 +38,14 @@ size_t inBinary(char binary[]) {
     return std::strlen(binary);
 }
 
-void isBinary(char binary[], int lenght) {
+bool isBinary(char binary[], int lenght) {
     for (int i = 0; i < lenght; i++) {
         if (binary[i] != '0' && binary[i] != '1') {
             std::cout << "Число не является двоичным" << std::endl;
-            return;
+            return false;
         }
     }
+    return true;
 }
 
 void addedBinary(char binary[], int lenght) {
@@ -52,16 +82,33 @@ int main() {
     char b[bitsize + 1];
     char output[bitsize + 1];
 
-    int a_lenght = inBinary(a);
-    int b_lenght = inBinary(b);
+    AB result = fileBinary(a, b, bitsize);
+    int a_lenght = result.a;
+    int b_lenght = result.b;
 
-    isBinary(a, a_lenght);
-    isBinary(b, b_lenght);
+    //int a_lenght = inBinary(a);
+    //int b_lenght = inBinary(b);
+
+    if (!isBinary(a, a_lenght) || !isBinary(b, b_lenght)) { return -1; }
 
     addedBinary(a, a_lenght);
     addedBinary(b, b_lenght);
 
     conjuction(bitsize, a, b, output);
+
+    std::ofstream out;
+    out.open("result.txt");
+
+    if (out.is_open())
+    {
+        out << "Конъюнкция введеных двоичных чисел:" << std::endl << std::endl;
+        out << a << std::endl;
+        out << b << std::endl;
+        out << "--------" << std::endl;
+        out << output << std::endl;
+    }
+
+    out.close();
 
     std::cout << std::endl << "Конъюнкция введеных двоичных чисел:" << std::endl << std::endl;
     std::cout << a << std::endl;
