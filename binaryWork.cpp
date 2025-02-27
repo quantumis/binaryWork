565925ed31c7bf8a3de7947317f91172bd687035
John Doe <Emaq-r@mail.ru>
2025-02-27 17:59:31 +0600

    rewrite a function

--- binaryWork.cpp
+++ binaryWork.cpp
@@ -1,61 +1,39 @@
-﻿#include <iostream> 
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-    setlocale(LC_ALL, "");
+size_t inBinary(char binary[]) {
+    std::cout << "Введите двоичное число (максимум 8 символов)" << std::endl;
+    std::cin >> binary;
 
-    const int bitsize = 8;
+    return std::strlen(binary);
+}
 
-    char a[bitsize + 1];
-    char b[bitsize + 1];
-    char output[bitsize + 1];
-
-    std::cout << "Введите первое двоичное число (максимум 8 символов)" << std::endl;
-    std::cin >> a;
-
-    int a_lenght = std::strlen(a);
-
-    std::cout << "Введите второе двоичное число (максимум 8 символов)" << std::endl;
-    std::cin >> b;
-
-    int b_lenght = std::strlen(b);
-
-    for (int i = 0; i < a_lenght; i++) {
-        if (a[i] != '0' && a[i] != '1') {
-            std::cout << "Первое число не является двоичным" << std::endl;
-            return 1;
+void isBinary(char binary[], int lenght) {
+    for (int i = 0; i < lenght; i++) {
+        if (binary[i] != '0' && binary[i] != '1') {
+            std::cout << "Число не является двоичным" << std::endl;
+            return;
         }
     }
+}
 
-    for (int i = 0; i < b_lenght; i++) {
-        if (b[i] != '0' && b[i] != '1') {
-            std::cout << "Второе число не является двоичным" << std::endl;
-            return 1;
+void addedBinary(char binary[], int lenght) {
+    if (lenght < 8) {
+        for (int i = lenght; i >= 0; i--) {
+            binary[i + 8 - lenght] = binary[i];
+        }
+
+        for (int i = 0; i < 8 - lenght; i++) {
+            binary[i] = '0';
         }
     }
+}
 
-    if (a_lenght < 8) {
-        for (int i = a_lenght; i >= 0; i--) {
-            a[i + 8 - a_lenght] = a[i];
-        }
-
-        for (int i = 0; i < 8 - a_lenght; i++) {
-            a[i] = '0';
-        }
-    }
-
-    if (b_lenght < 8) {
-        for (int i = b_lenght; i >= 0; i--) {
-            a[i + 8 - b_lenght] = a[i];
-        }
-
-        for (int i = 0; i < 8 - b_lenght; i++) {
-            b[i] = '0';
-        }
-    }
-
+void conjuction(int bitsize, char binary_a[], char binary_b[], char output[]) {
     for (int i = 0; i < bitsize; i++)
     {
-        if (a[i] == '1' && b[i] == '1') {
+        if (binary_a[i] == '1' && binary_b[i] == '1') {
             output[i] = '1';
         }
         else {
@@ -64,6 +42,26 @@ int main() {
     }
 
     output[bitsize] = 0;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    const int bitsize = 8;
+    char a[bitsize + 1];
+    char b[bitsize + 1];
+    char output[bitsize + 1];
+
+    int a_lenght = inBinary(a);
+    int b_lenght = inBinary(b);
+
+    isBinary(a, a_lenght);
+    isBinary(b, b_lenght);
+
+    addedBinary(a, a_lenght);
+    addedBinary(b, b_lenght);
+
+    conjuction(bitsize, a, b, output);
 
     std::cout << std::endl << "Конъюнкция введеных двоичных чисел:" << std::endl << std::endl;
     std::cout << a << std::endl;
