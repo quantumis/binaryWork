737815a12dcfd93e31626fd15338a0c249ad6692
quantumis <Emaq-r@mail.ru>
2025-03-30 20:33:27 +0600

    rewrite on function
    rewrite on function

--- binaryWork.py
+++ binaryWork.py
@@ -1,17 +1,27 @@
+def is_binary(arr):
+    return all(x in {'0', '1'} for x in arr)
+def added_binary(a, len):
+    return ['0'] * (len - len(a)) + a
+def conjuction(a, b):
+    return ['1' if x == '1' and y == '1' else '0' for x, y in zip(a, b)]
+
+
 a = list(input().strip())
 b = list(input().strip())
 out = []
 
-if not all(x in {'0', '1'} for x in a):
+if not is_binary(a):
     print("Первое число не является двоичным")
-if not all(x in {'0', '1'} for x in b):
+    exit(-1)
+if not is_binary(b):
     print("Второе число не является двоичным")
+    exit(-1)
 
 max_len = max(len(a), len(b))
-a = ['0'] * (max_len - len(a)) + a
-b = ['0'] * (max_len - len(b)) + b
+a = added_binary(a, max_len)
+b = added_binary(b, max_len)
 
-out = ['1' if x == '1' and y == '1' else '0' for x, y in zip(a, b)]
+out = conjuction(a, b)
 
 print(''.join(a))
 print(''.join(b))
