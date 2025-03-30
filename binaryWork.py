b933e3563d6c7d82e303108cc06f17b26be23242
quantumis <Emaq-r@mail.ru>
2025-03-30 22:02:18 +0600

    complete Py commit

--- binaryWork.py
+++ binaryWork.py
@@ -1,55 +1,55 @@
-def is_binary(arr):
-    return all(x in {'0', '1'} for x in arr)
-def added_binary(a, length):
-    return ['0'] * (length - len(a)) + a
-def conjunction(a, b):
-    return ['1' if x == '1' and y == '1' else '0' for x, y in zip(a, b)]
-def output(a, b, c):
-    return f"{''.join(a)}\n{''.join(b)}\n{'-'*len(c)}\n{''.join(c)}"
-def get_binary_input():
-    value = input("Введите число: ").strip()
-    if is_binary(value):
-        return list(value)
-    print("Число не является двоичным")
-    exit(-1)
-def read_from_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            data = file.readline().strip()
-            if not is_binary(data):
+class Binary:
+    def __init__(self, str= None):
+        if str is not None:
+            if all(x in '01' for x in str):
+                self.value = list(str)
+            else:
                 print("Число не является двоичным")
                 exit(-1)
-            return list(data)
-    except:
-        print(f"Ошибка при чтении файла")
-        exit(-1)
+        else:
+            self.value = []
+    def __str__(self):
+        return ''.join(self.value)
+
+    def added_binary(self, length):
+        self.value = ['0'] * (length - len(self.value)) + self.value
+    def conjunction(self, b):
+        temp = Binary()
+        temp.value = ['1' if x == '1' and y == '1' else '0' for x, y in zip(self.value, b.value)]
+        return temp
+
+def output(a, b, c):
+    return f"{a}\n{b}\n{'-'*len(c.value)}\n{c}"
 def dialog_event():
     print("Выберите желаемое действие (соответствующую цифру в консоль):")
     print("1. Работа в консоли")
     print("2. Работа с файлом")
-    choice = input().strip()
+    choice = input()
 
     if choice == '1':
-        return get_binary_input()
+        return Binary(input("Введите число: "))
     elif choice == '2':
-        return read_from_file("binary.txt")
+        try:
+            with open('binary.txt', 'r') as file:
+                return Binary(file.readline())
+        except:
+            print("Ошибка при чтении файла")
+            exit(-1)
     else:
-        print("Неверное действие. Попробуйте еще раз")
+        print("Неверный выбор")
         exit(-1)
-
 
 if __name__ == "__main__":
     a = dialog_event()
     b = dialog_event()
 
-    max_len = max(len(a), len(b))
-    a = added_binary(a, max_len)
-    b = added_binary(b, max_len)
+    max_len = max(len(a.value), len(b.value))
+    a.added_binary(max_len)
+    b.added_binary(max_len)
 
-    out = conjunction(a, b)
+    out = a.conjunction(b)
 
     print(output(a, b, out))
-
     try:
         with open('result.txt', 'w') as file:
             file.write(output(a, b, out))
