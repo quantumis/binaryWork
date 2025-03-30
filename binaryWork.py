2883c7454e593b6cb01f108a559c76fbabebe5fb
quantumis <Emaq-r@mail.ru>
2025-03-30 21:02:46 +0600

    add console menu

--- binaryWork.py
+++ binaryWork.py
@@ -6,23 +6,41 @@ def conjunction(a, b):
     return ['1' if x == '1' and y == '1' else '0' for x, y in zip(a, b)]
 def output(a, b, c):
     return f"{''.join(a)}\n{''.join(b)}\n{'-'*len(c)}\n{''.join(c)}"
+def get_binary_input():
+    value = input("Введите число: ").strip()
+    if is_binary(value):
+        return list(value)
+    print("Число не является двоичным")
+    exit(-1)
+def read_from_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = file.readline().strip()
+            if not is_binary(data):
+                print("Число не является двоичным")
+                exit(-1)
+            return list(data)
+    except:
+        print(f"Ошибка при чтении файла")
+        exit(-1)
+def dialog_event():
+    print("Выберите желаемое действие (соответствующую цифру в консоль):")
+    print("1. Работа в консоли")
+    print("2. Работа с файлом")
+    choice = input().strip()
+
+    if choice == '1':
+        return get_binary_input()
+    elif choice == '2':
+        return read_from_file("binary.txt")
+    else:
+        print("Неверное действие. Попробуйте еще раз")
+        exit(-1)
+
 
 if __name__ == "__main__":
-    a = list(input("Введите первое двоичное число: ").strip())
-
-    try:
-        with open('binary.txt', 'r') as file:
-            b = list(file.readline().strip())
-    except:
-        print("Ошибка при чтении файла")
-        exit(-1)
-
-    if not is_binary(a):
-        print("Первое число не является двоичным")
-        exit(-1)
-    if not is_binary(b):
-        print("Второе число не является двоичным")
-        exit(-1)
+    a = dialog_event()
+    b = dialog_event()
 
     max_len = max(len(a), len(b))
     a = added_binary(a, max_len)
